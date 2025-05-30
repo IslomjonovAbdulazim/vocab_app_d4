b0c3ff37e30f6b9b34ab49e14e6c1ed4f9c923ac
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-30 18:08:04 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:budget_tracker_app_d4/pages/new_folder_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +14,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("All Folders"),
+        actions: [
+          CupertinoButton(
+            onPressed: () async {
+              await Get.to(NewFolderPage());
+            },
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
