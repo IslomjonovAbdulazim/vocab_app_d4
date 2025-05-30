b7cd7ad575f69cb799731524f2166fc26cd37b5c
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-30 18:03:53 +0500

    imp

--- lib/models/vocab_model.dart
+++ lib/models/vocab_model.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'folder_model.dart';
+
 class VocabModel {
   late String title;
   late String description;
@@ -25,13 +31,49 @@ class VocabModel {
   }
 
   Map toJson() => {
-    "title": title,
-    "description": description,
-    "createdAt": createdAt.toIso8601String(),
-    "folderId": folderId,
-    "corrects": corrects,
-    "wrongs": wrongs,
-  };
+        "title": title,
+        "description": description,
+        "createdAt": createdAt.toIso8601String(),
+        "folderId": folderId,
+        "corrects": corrects,
+        "wrongs": wrongs,
+      };
 }
 
+Future<void> saveAllVocabs(List<VocabModel> vocabs) async {
+  final jsonList = vocabs.map((model) => model.toJson()).toList();
+  final data = jsonEncode(jsonList);
+  final db = await SharedPreferences.getInstance();
+  await db.setString("vocabs", data);
+}
 
+Future<List<VocabModel>> getAllVocabs() async {
+  final db = await SharedPreferences.getInstance();
+  final data = db.getString("vocabs") ?? "[]";
+  final jsonList = List.from(jsonDecode(data));
+  final result = jsonList.map((json) => VocabModel.fromJson(json)).toList();
+  result.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+  return result;
+}
+
+Future<void> addNewVocab(VocabModel vocab, FolderModel folder) async {
+  final allVocabs = await getAllVocabs();
+  allVocabs.add(vocab);
+  await saveAllVocabs(allVocabs);
+  final allFolders = await getAllFolders();
+  folder.totalWords++;
+  allFolders.removeWhere((model) => folder.createdAt == model.createdAt);
+  allFolders.add(folder);
+  await saveAllFolders(allFolders);
+}
+
+Future<void> deleteVocab(VocabModel vocab, FolderModel folder) async {
+  final allVocabs = await getAllVocabs();
+  allVocabs.removeWhere((model) => model.createdAt == vocab.createdAt);
+  await saveAllVocabs(allVocabs);
+  final allFolders = await getAllFolders();
+  folder.totalWords--;
+  allFolders.removeWhere((model) => folder.createdAt == model.createdAt);
+  allFolders.add(folder);
+  await saveAllFolders(allFolders);
+}
